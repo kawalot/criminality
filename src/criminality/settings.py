50b6e0c79df2e958cbc61460099300f3dc2f8a8b
AleksandrPeshcherov <kawalot@gmail.com>
2019-11-11 15:56:00 +0200

    serving static files

--- src/criminality/settings.py
+++ src/criminality/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'criminality.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,11 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "static-serve")
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media-serve")
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "criminality", "static"),
+    os.path.join(BASE_DIR, "static"),
     # '/var/www/criminality/static',
 ]
 
--- src/criminality/urls.py
+++ src/criminality/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+from .views import IndexView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('', views.home, name='home_page'),
+    path('', IndexView.as_view(), name='home_page'),
     path('api/', include('cases.api.urls')),
 ]
 
