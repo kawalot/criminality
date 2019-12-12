377e09cb4c1310a1202550f1d28566c110524f71
AleksandrPeshcherov <kawalot@gmail.com>
2019-12-12 14:14:27 +0200

    add font fira sans, favicon

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,14 @@
 import React from "react";
-import '../styles/Header.scss'
 import { NavLink } from 'react-router-dom'
+import '../styles/Header.scss'
+
 
 const Header = () => {
     return <header className="header">
 
             <div className="header-logo">Criminality map</div>
             <nav className="header-nav">
-                <NavLink to='/' exact activeClassName="active" className="header-nav__link">Home<span className="nav-span"></span></NavLink>
+                <NavLink to='/' exact activeClassName="active" className="header-nav__link">Home</NavLink>
                 <NavLink to='/map' className="header-nav__link">Map</NavLink>
                 <NavLink to='/about' className="header-nav__link">About</NavLink>
                 <NavLink to='/login' className="header-nav__link">Login</NavLink>
